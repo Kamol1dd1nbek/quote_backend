f1869a8f2ae029b5443ea79ea372f29b75dd1afa
Kamoliddinbek <kamoliddinxudoyberdiyev72@gmail.com>
2023-10-01 10:11:46 +0500

    app basic settings and prisma connecting

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,19 @@
+import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as cookieParser from 'cookie-parser';
 
-async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
-}
-bootstrap();
+const start = async () => {
+  try {
+    const app = await NestFactory.create(AppModule);
+    app.use(cookieParser());
+    app.useGlobalPipes(new ValidationPipe());
+    const PORT = process.env.PORT || 3001;
+    app.listen(PORT, () => {
+      console.log(`Server is runnig on port: ${PORT}`);
+    });
+  } catch (error) {
+    console.log(error);
+  }
+};
+start();
---
+++ src/prisma/prisma.service.ts
@@ -0,0 +1,25 @@
+import { Injectable, OnModuleDestroy, OnModuleInit } from '@nestjs/common';
+import { PrismaClient } from '@prisma/client';
+
+@Injectable()
+export class PrismaService
+  extends PrismaClient
+  implements OnModuleInit, OnModuleDestroy
+{
+  constructor() {
+    super({
+      datasources: {
+        db: {
+          url: 'postgresql://postgres:06062006Kamoliddin@localhost:5432/quote-app?schema=public',
+        },
+      },
+    });
+  }
+
+  async onModuleInit() {
+    await this.$connect();
+  }
+  async onModuleDestroy() {
+    await this.$disconnect();
+  }
+}
